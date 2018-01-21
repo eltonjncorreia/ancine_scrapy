79db38baa0f8365f9b4d4492a42fae29893de8e0
elton jefferson <eltonjncorreia@gmail.com>
2018-01-21 16:01:57 -0300

    refactoring

--- brasil_scrapy/brasil_scrapy/spiders/ancine.py
+++ brasil_scrapy/brasil_scrapy/spiders/ancine.py
@@ -5,10 +5,8 @@ import scrapy
 class AncineSpider(scrapy.Spider):
     name = 'ancine'
 
-    url = 'https://www.ancine.gov.br/pt-br/brasil-nas-telas'
-
     allowed_domains = ['ancine.gov.br']
-    start_urls = [url]
+    start_urls = ['https://www.ancine.gov.br/pt-br/brasil-nas-telas']
 
     def parse(self, response):
         items = response.xpath('//*[@id="conteudo"]/div/div/table/tbody/tr')
@@ -16,36 +14,47 @@ class AncineSpider(scrapy.Spider):
         for item in items:
             urls = item.xpath('.//td/h3/a/@href').extract_first()
 
-
             yield scrapy.Request('https://www.ancine.gov.br{}'.format(urls), callback=self.detail)
 
         next_page = response.xpath(
-            '//*[@id="conteudo"]/div//ul/li//a/@href'
+                    '//*[@id="conteudo"]/div//ul/li//a/@href'
         ).extract_first()
 
         if next_page:
             yield scrapy.Request('https://www.ancine.gov.br{}'.format(next_page), callback=self.parse)
 
-
     def detail(self, response):
-        self.log(response.url)
-        title = response.xpath(
-            './/div[contains(@class, "content-header")]//h2/text()'
-        ).extract_first()
+            yield {
+                'title': response.css(
+                    'div.content-header h2::text').extract_first(),
 
-        sinopse = response.xpath(
-            './/tr/td/b/text()'
-        ).extract_first()
+                'sinopse': response.css(
+                    'div.field-item p::text').extract_first(),
 
-        producao = response.xpath(
-            './/td[2]/div/div/div/text()'
-        ).extract_first()
+                'image': response.xpath(
+                    './/div//table//a/img/@src').extract_first()
 
-        yield {
-            'title': title,
-            'sinopse': sinopse,
-            'producao': producao,
-        }
+            }
+        # title = response.xpath('.//div[contains(@class, "content-header")]//h2/text()').extract_first()
+
+        # sinopse = response.xpath(
+        #     './/div[2]//td/div//p/text()'
+        # ).extract_first()
+
+        #
+        # producao = response.xpath(
+        #     './/td[2]/div/div/div/text()'
+        # ).extract_first()
+        #
+        # coproducao = response.xpath(
+        #     './/div[2]/table//tr[3]/td[2]//div/text()'
+        # ).extract_first()
+        #
+        # yield {
+        #     'title': title,
+        #     'sinopse': sinopse,
+        #     'producao': producao,
+        # }
 
 
 
