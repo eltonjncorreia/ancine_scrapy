18089c3fa495b8a5b0a0dc9484fda4d6474c688e
elton jefferson <eltonjncorreia@gmail.com>
2018-01-21 12:19:16 -0300

    testing item, placing logs

--- brasil_scrapy/brasil_scrapy/items.py
+++ brasil_scrapy/brasil_scrapy/items.py
@@ -11,4 +11,5 @@ import scrapy
 class BrasilScrapyItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    scrapy.Item.log('----- capturado _-----')
+
