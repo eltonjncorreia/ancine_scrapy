27d3407018c94c8e03f472cf28e6c0317553a649
elton jefferson <eltonjncorreia@gmail.com>
2018-01-22 01:03:03 -0300

    writing documentation

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# ancine_scrapy
-Lista de filmes brasileiro da pagina Agencia Nacional de Cinema
+# Ancine - Agência Nacional do Cinema
+
+###  Brazilian movies,
+#### Listed on the Agencia Nacional de Cinema
+
+## page link
+
+```bash
+
+https://www.ancine.gov.br/pt-br/brasil-nas-telas
+
+```
+
+## with using the project
+### recommended to create a virtual environment,
+### but can be ignored if you already know what you are doing
+
+```bash
+
+git clone https://github.com/eltonjncorreia/ancine_scrapy.git
+cd ancine_scrapy
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+cd brasil_scrapy
+scrapy crawl ancine
+
+```
+
--- brasil_scrapy/brasil_scrapy/spiders/ancine.py
+++ brasil_scrapy/brasil_scrapy/spiders/ancine.py
@@ -3,27 +3,37 @@ import scrapy
 
 
 class AncineSpider(scrapy.Spider):
+    # spider's name
     name = 'ancine'
-
+    # domain
     allowed_domains = ['ancine.gov.br']
+    # url name
     start_urls = ['https://www.ancine.gov.br/pt-br/brasil-nas-telas']
 
+    # parse
     def parse(self, response):
+        # is capturing the links, through the table
         items = response.xpath('//*[@id="conteudo"]/div/div/table/tbody/tr')
 
+        # is capturing href and passes the captured items to the method
         for item in items:
             urls = item.xpath('.//td/h3/a/@href').extract_first()
 
             yield scrapy.Request('https://www.ancine.gov.br{}'.format(urls), callback=self.detail)
 
+        # advanced on page
         next_page = response.xpath(
                     '//*[@id="conteudo"]/div//ul/li//a/@href'
         ).extract_first()
 
+        # takes only the href and goes to the proper function parse()
         if next_page:
             yield scrapy.Request('https://www.ancine.gov.br{}'.format(next_page), callback=self.parse)
 
+    # receives the response and displays the data
     def detail(self, response):
+            # was used .css, because I found it simpler and readable,
+            # but the xpath "" could be used.
             yield {
                 'title': response.css(
                     'div.content-header h2::text').extract_first(),
@@ -35,28 +45,3 @@ class AncineSpider(scrapy.Spider):
                     './/div//table//a/img/@src').extract_first()
 
             }
-        # title = response.xpath('.//div[contains(@class, "content-header")]//h2/text()').extract_first()
-
-        # sinopse = response.xpath(
-        #     './/div[2]//td/div//p/text()'
-        # ).extract_first()
-
-        #
-        # producao = response.xpath(
-        #     './/td[2]/div/div/div/text()'
-        # ).extract_first()
-        #
-        # coproducao = response.xpath(
-        #     './/div[2]/table//tr[3]/td[2]//div/text()'
-        # ).extract_first()
-        #
-        # yield {
-        #     'title': title,
-        #     'sinopse': sinopse,
-        #     'producao': producao,
-        # }
-
-
-
-
-
