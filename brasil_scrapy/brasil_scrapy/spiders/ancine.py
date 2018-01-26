dc0af582336f3eb21f37a6b092bad31c3e366d86
elton jefferson <eltonjncorreia@gmail.com>
2018-01-26 14:25:00 -0300

    refactoring

--- brasil_scrapy/brasil_scrapy/spiders/ancine.py
+++ brasil_scrapy/brasil_scrapy/spiders/ancine.py
@@ -2,6 +2,7 @@ import scrapy
 
 
 class AncineSpider(scrapy.Spider):
+
     name = 'ancine'
 
     allowed_domains = ['ancine.gov.br']
@@ -17,28 +18,16 @@ class AncineSpider(scrapy.Spider):
         for div in response.css('div.item-list li'):
             next_page = div.css('a::attr(href)').extract_first()
             if next_page:
-                yield scrapy.Request('https://www.ancine.gov.br{}'.format(next_page),
-                                 callback=self.parse)
+                yield scrapy.Request('https://www.ancine.gov.br{}'.format(next_page), callback=self.parse)
 
     def detail(self, response):
-        # yield {
-        #     'title': response.css('h3 a::text').extract_first(),
-        #     'sinopse': response.css('td.views-field p::text').extract_first(),
-        #     'producao': response.css('td b::text').extract_first()
-        # }
 
         yield {
             'title': response.css('div.content-header h2::text').extract_first(),
-
             'sinopse': response.css('div.field-item p::text').extract_first(),
-
             'produção': response.css('div.field-name-field-produtora div::text').extract_first(),
-
             'genero': response.css('div.field-name-field-genero div::text').extract_first(),
-
             'data-lancamento': response.css('span.date-display-single::text').extract_first(),
-
             'image': response.css('table.tb-detalhes td img::attr(src)').extract_first()
-
         }
 
